8e027b38cb3bcbd1149fe44d7102e708bfb6affa
gmourar <gabrielmourarocha@gmail.com>
2024-09-30 12:09:07 -0300

    requisicao -  itens checklist - ok

--- src/components/CheckList.js
+++ src/components/CheckList.js
@@ -1,26 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Container, Row, Col, Card, Form } from 'react-bootstrap';
-import axios from 'axios'
-
-
+import axios from 'axios';
 
 const CheckList = () => {
   const [selectedItems, setSelectedItems] = useState({});
   const [descriptions, setDescriptions] = useState({});
-  const [userList , setUserList] = useState([])
-  const [userNames , setUsernames] = useState(null)
+  const [checklistItems, setChecklistItems] = useState([]); 
+  const [title, setTitle] = useState('');
+  
+  const ip = 'http://26.121.130.48:8080';
+  const check_id = 'cc21b860-db95-4386-b12c-285aeb756700'
 
-  const ip = 'http://26.121.130.48:8080'
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get(`${ip}/api/users `);
+        const res = await axios.get(`${ip}/api/checklists/${check_id}`);
         console.log('Resposta recebida:', res.data);
-        setUserList(res.data); 
-
-        const usernames = res.data.map(user => user.username);
-        setUsernames(usernames);
+        
+        setTitle(res.data.title); 
+        setChecklistItems(res.data.items); 
       } catch (error) {
         console.error('Erro ao buscar dados:', error);
       }
@@ -29,30 +28,17 @@ const CheckList = () => {
     fetchData();
   }, []);  
 
-
-  const checklistItems = [
-    "Teclado",
-    "Tapete",
-    "Travesseiro",
-    "Cama",
-    "Computador",
-    "Guarda-roupa",
-    "Janela",
-    "Mesa",
-    "Monitor"
-  ];
-
-  const handleItemClick = (item, status) => {
+  const handleItemClick = (itemId, status) => {
     setSelectedItems(prevState => ({
       ...prevState,
-      [item]: status
+      [itemId]: status
     }));
   };
 
-  const handleDescriptionChange = (item, text) => {
+  const handleDescriptionChange = (itemId, text) => {
     setDescriptions(prevState => ({
       ...prevState,
-      [item]: text
+      [itemId]: text
     }));
   };
 
@@ -60,12 +46,10 @@ const CheckList = () => {
     const isConfirmed = window.confirm("Tem certeza que deseja enviar o checklist?");
     
     if (isConfirmed) {
-     
       console.log('Itens selecionados:', selectedItems);
       console.log('Descrições:', descriptions);
       alert("Checklist enviado!");
     } else {
-      
       alert("Envio cancelado.");
     }
   };
@@ -74,47 +58,46 @@ const CheckList = () => {
     <Container className="d-flex justify-content-center align-items-center vh-100">
       <Card style={{ width: '100%', maxWidth: '500px', height: '90vh' }} className="p-6 shadow-lg rounded">
         <Card.Header className="text-center bg-light text-dark rounded">
-          <h4>Checklist do Quarto</h4>
-      <h6>{userNames}</h6>
+          <h4>{title}</h4> 
         </Card.Header>
 
         <Card.Body style={{ overflowY: 'auto' }}>
           {checklistItems.map((item, index) => (
-            <div key={index} className="mb-2 p-4  rounded" style={{ backgroundColor: '#f8f9fa' }}>
+            <div key={item.id} className="mb-2 p-4  rounded" style={{ backgroundColor: '#f8f9fa' }}>
               <Row className="align-items-center">
                 <Col xs={6}>
-                  <span className="fs-5 fw-semibold">{item}</span>
+                  <span className="fs-5 fw-semibold">{item.name}</span> 
                 </Col>
                 <Col xs={3} className="text-center">
                   <Button
                     variant="outline-danger"
-                    className={`rounded${selectedItems[item] === 'unchecked' ? 'border-danger bg-danger text-white' : ''}`}
+                    className={`rounded${selectedItems[item.id] === 'unchecked' ? 'border-danger bg-danger text-white' : ''}`}
                     style={{ width: '60px', height: '50px' }}
-                    onClick={() => handleItemClick(item, 'unchecked')}
+                    onClick={() => handleItemClick(item.id, 'unchecked')}
                   >
                     
                   </Button>
                 </Col>
-                <Col xs={2} className="text-center">
+                <Col xs={3} className="text-center">
                   <Button
                     variant="outline-success"
-                    className={`rounded ${selectedItems[item] === 'checked' ? 'border-success bg-success text-white' : ''}`}
+                    className={`rounded ${selectedItems[item.id] === 'checked' ? 'border-success bg-success text-white' : ''}`}
                     style={{ width: '60px', height: '50px' }}
-                    onClick={() => handleItemClick(item, 'checked')}
+                    onClick={() => handleItemClick(item.id, 'checked')}
                   >
-                    
+                   
                   </Button>
                 </Col>
               </Row>
 
-              {/* Mostrar campo de descrição se "unchecked" for selecionado */}
-              {selectedItems[item] === 'unchecked' && (
+              
+              {selectedItems[item.id] === 'unchecked' && (
                 <Form.Group className="mt-3">
                   <Form.Control
                     type="text"
                     placeholder="Descreva o motivo"
-                    value={descriptions[item] || ''}
-                    onChange={(e) => handleDescriptionChange(item, e.target.value)}
+                    value={descriptions[item.id] || ''}
+                    onChange={(e) => handleDescriptionChange(item.id, e.target.value)}
                     className="shadow-sm"
                   />
                 </Form.Group>
@@ -136,4 +119,3 @@ const CheckList = () => {
 };
 
 export default CheckList;
-
